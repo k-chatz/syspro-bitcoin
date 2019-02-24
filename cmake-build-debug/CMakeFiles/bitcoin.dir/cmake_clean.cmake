7f8933184d7a4b914b137d31a5658972941c41f4
Costas Chatzopoulos <kwstarikanos@gmail.com>
2019-02-24 00:23:20 -0800

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/bitcoin.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bitcoin.dir/main.c.o"
+  "bitcoin.pdb"
+  "bitcoin"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/bitcoin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
