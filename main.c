c38b04b51437f7f5647b6633a756416424b706e3
Costas Chatzopoulos <kwstarikanos@gmail.com>
2019-02-27 20:05:09 +0200

    read options now is in a function.

--- main.c
+++ main.c
@@ -2,109 +2,104 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define LINE_SIZE 256
 
 void wrongOptionValue(char *opt, char *val);
 
+void readOptions(int argc, char **argv, char **a, char **t, int *v, int *h1, int *h2, int *b);
+
 int main(int argc, char *argv[]) {
-    int i, bucketSize = 0, bitCoinValue = 0, senderHashtableNumOfEntries = 0, receiverHashtableNumOfEntries = 0;
-    char *opt, *optVal, *bitCoinBalancesFile = NULL, *transactionsFile = NULL;
-    for (i = 1; i < argc; ++i) {
-        opt = argv[i];
-        optVal = argv[i + 1];
+    int i, b = 0, v = 0, h1 = 0, h2 = 0;
+    char  buf[LINE_SIZE], *opt, *optVal, *a = NULL, *t = NULL, *token = NULL;
 
-        if (strcmp(opt, "-a") == 0) {
-            if (optVal != NULL && optVal[0] != '-') { /*bitCoinBalancesFile*/
-                bitCoinBalancesFile = optVal;
-            } else {
-                wrongOptionValue(opt, optVal);
-            }
-        } else if (strcmp(opt, "-t") == 0) { /*transactionsFile*/
-            if (optVal != NULL && optVal[0] != '-') {
-                transactionsFile = optVal;
-            } else {
-                wrongOptionValue(opt, optVal);
-            }
-        } else if (strcmp(opt, "-v") == 0) { /*bitCoinValue*/
-            if (optVal != NULL && optVal[0] != '-') {
-                bitCoinValue = atoi(optVal);
-            } else {
-                wrongOptionValue(opt, optVal);
-            }
-        } else if (strcmp(opt, "-h1") == 0) { /*senderHashtableNumOfEntries*/
-            if (optVal != NULL && optVal[0] != '-') {
-                senderHashtableNumOfEntries = atoi(optVal);
-            } else {
-                wrongOptionValue(opt, optVal);
-            }
-        } else if (strcmp(opt, "-h2") == 0) { /*receiverHashtableNumOfEntries*/
-            if (optVal != NULL && optVal[0] != '-') {
-                receiverHashtableNumOfEntries = atoi(optVal);
-            } else {
-                wrongOptionValue(opt, optVal);
-            }
-        } else if (strcmp(opt, "-b") == 0) { /*bucketSize*/
-            if (optVal != NULL && optVal[0] != '-') {
-                bucketSize = atoi(optVal);
-            } else {
-                wrongOptionValue(opt, optVal);
-            }
-        }
-    }
+    /*Read argument options from command line*/
+    readOptions(argc, argv, &a, &t, &v, &h1, &h2, &b);
 
-    printf("bitCoinBalancesFile: %s\n", bitCoinBalancesFile);
-    printf("transactionsFile: %s\n", transactionsFile);
-    printf("bitCoinValue: %d\n", bitCoinValue);
-    printf("senderHashtableNumOfEntries: %d\n", senderHashtableNumOfEntries);
-    printf("receiverHashtableNumOfEntries: %d\n", receiverHashtableNumOfEntries);
-    printf("bucketSize: %d\n", bucketSize);
+    printf("a: %s\n", a);
+    printf("t: %s\n", t);
+    printf("v: %d\n", v);
+    printf("h1: %d\n", h1);
+    printf("h2: %d\n", h2);
+    printf("b: %d\n", b);
 
+    FILE *fp = fopen(a, "r");
+    if (fp != NULL) {
 
-    FILE *pFile = fopen(bitCoinBalancesFile, "r");
-    if (pFile != NULL) {
+/*        while (fgets(buf, LINE_SIZE, fp) != NULL) {
+            token = strtok(buf + 1, ",");
 
-        int nbytes = 1024;
-        char *line = (char *) malloc(nbytes);
-        char *buf = (char *) malloc(nbytes);
+            token = strtok(NULL, ",");
 
-        size_t bytes_read;
-        int linesize = 0;
-        while (fgets(buf, nbytes, pFile) != NULL) {
-            bytes_read = strlen(buf);
-            // if line length larger than size of line buffer
-            if (linesize + bytes_read > nbytes) {
-                char *tmp = line;
-                nbytes += nbytes / 2;
-                line = (char *) malloc(nbytes);
-                memcpy(line, tmp, linesize);
-                free(tmp);
-            }
-            memcpy(line + linesize, buf, bytes_read);
-            linesize += bytes_read;
+            token++;
+            token[strlen(token) - 1] = 0;
+            strncpy(primaryRecord.name, token, sizeof(primaryRecord.name));
 
-            if (feof(pFile) || buf[bytes_read - 1] == '\n') {
+            token = strtok(NULL, ",");
+            token++;
+            token[strlen(token) - 1] = 0;
+            strncpy(primaryRecord.surname, token, sizeof(primaryRecord.surname));
 
-                printf("line: %s", line);
-                //handle_line(line);
-                linesize = 0;
-                memset(line, '\0', nbytes);
-            }
-        }
+            token = strtok(NULL, "}");
+            strncpy(primaryRecord.address, token, sizeof(primaryRecord.address));
 
-        free(buf);
-        free(line);
-
-        printf("\n");
+            HT_InsertEntry(info, primaryRecord);
+        }*/
 
     } else {
         fprintf(stderr, "File '%s' doesn't exists!\n", argv[i + 1]);
         exit(1);
     }
 
-
     return EXIT_SUCCESS;
+}
+
+void readOptions(int argc, char **argv, char **a, char **t, int *v, int *h1, int *h2, int *b) {
+    int i;
+    char *opt, *optVal;
+    for (i = 1; i < argc; ++i) {
+        opt = argv[i];
+        optVal = argv[i + 1];
+        if (strcmp(opt, "-a") == 0) {
+            if (optVal != NULL && optVal[0] != '-') { /*bitCoinBalancesFile*/
+                *a = optVal;
+            } else {
+                wrongOptionValue(opt, optVal);
+            }
+        } else if (strcmp(opt, "-t") == 0) { /*transactionsFile*/
+            if (optVal != NULL && optVal[0] != '-') {
+                *t = optVal;
+            } else {
+                wrongOptionValue(opt, optVal);
+            }
+        } else if (strcmp(opt, "-v") == 0) { /*bitCoinValue*/
+            if (optVal != NULL && optVal[0] != '-') {
+                *v = atoi(optVal);
+            } else {
+                wrongOptionValue(opt, optVal);
+            }
+        } else if (strcmp(opt, "-h1") == 0) { /*senderHashtableNumOfEntries*/
+            if (optVal != NULL && optVal[0] != '-') {
+                *h1 = atoi(optVal);
+            } else {
+                wrongOptionValue(opt, optVal);
+            }
+        } else if (strcmp(opt, "-h2") == 0) { /*receiverHashtableNumOfEntries*/
+            if (optVal != NULL && optVal[0] != '-') {
+                *h2 = atoi(optVal);
+            } else {
+                wrongOptionValue(opt, optVal);
+            }
+        } else if (strcmp(opt, "-b") == 0) { /*bucketSize*/
+            if (optVal != NULL && optVal[0] != '-') {
+                *b = atoi(optVal);
+            } else {
+                wrongOptionValue(opt, optVal);
+            }
+        }
+    }
 }
 
 void wrongOptionValue(char *opt, char *val) {
     fprintf(stderr, "Wrong value [%s] for option '%s'\n", val, opt);
-    exit(1);
+    exit(EXIT_FAILURE);
 }
